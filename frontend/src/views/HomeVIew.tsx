04f431acef49447d9325bdbf64c0e3b232227324
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-10-25 15:34:33 +0200

    feat(frontend/navbar): increase size of buttons and add login/register buttons

--- frontend/src/views/HomeVIew.tsx
+++ frontend/src/views/HomeVIew.tsx
@@ -1,19 +1,48 @@
 import { Button } from "@/components/ui/button";
-import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import { Menubar, MenubarMenu } from "@/components/ui/menubar";
 import useAppState from "@/store/state";
-import { MoonIcon, SunIcon } from "lucide-react";
+import { LogIn, MoonIcon, SunIcon, UserIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function HomeView() {
   const state = useAppState();
+  const navigate = useNavigate();
 
   return (
     <>
-      <Menubar>
+      <Menubar className="h-14">
         <div className="flex-grow h-auto" id="menubar-spacer" />
+
         <MenubarMenu>
-          <MenubarTrigger onClick={state.toggleMode}>
+          <>
+            {/* Register and Login buttons */}
+            <Button
+              className="my-1 px-1 py-1 h-12 flex flex-row text-base [&_svg]:size-6"
+              variant={"outline"}
+              onClick={() => navigate("/register")}
+            >
+              <UserIcon />
+              <p>Register</p>
+            </Button>
+            <Button
+              className="my-1 px-1 py-1 h-12 flex flex-row text-base [&_svg]:size-6"
+              variant={"outline"}
+              onClick={() => navigate("/login")}
+            >
+              <LogIn />
+              <p>Login</p>
+            </Button>
+          </>
+        </MenubarMenu>
+
+        <MenubarMenu>
+          <Button
+            className="my-1 px-1 py-1 w-12 h-12 flex flex-row text-base [&_svg]:size-6"
+            variant={"outline"}
+            onClick={state.toggleMode}
+          >
             {state.mode === "light" ? <MoonIcon /> : <SunIcon />}
-          </MenubarTrigger>
+          </Button>
         </MenubarMenu>
       </Menubar>
       <h1 className="text-3xl font-bold underline">Hello from HomeHub!</h1>
