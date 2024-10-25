b23b575040be320daef34bc5da81bdfd6d3ca8b6
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-10-25 08:18:44 +0200

    chore: setup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello from HomeHub!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello from HomeHub!</h1>
+      <Button>Click me</Button>
+    </>
+  );
 }
 
 export default App;
