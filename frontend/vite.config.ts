541b010abe44fb9412d87adf321b4701322730ab
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-10-25 08:30:00 +0200

    chore(frontend): add rollup build visualizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "./stats/treemap.html",
+      template: "treemap",
+    }),
+    visualizer({
+      filename: "./stats/flamegraph.html",
+      template: "flamegraph",
+    }),
+    visualizer({
+      filename: "./stats/network.html",
+      template: "network",
+    }),
+    visualizer({
+      filename: "./stats/sunburst.html",
+      template: "sunburst",
+    }),
+  ],
 
   resolve: {
     alias: {
