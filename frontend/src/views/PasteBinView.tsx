63f6fdee6237d12ca3171f9f743cca5be0424d77
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-10-28 10:18:22 +0100

    feat(frontend): add input for paste title

--- frontend/src/views/PasteBinView.tsx
+++ frontend/src/views/PasteBinView.tsx
@@ -1,13 +1,19 @@
 import Menubar from "@/components/menubar";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { useRef } from "react";
 
 function PasteBinView() {
+  const titleRef = useRef<HTMLInputElement>(null);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleSubmit = () => {
     if (textAreaRef.current == null) {
+      return;
+    }
+
+    if (titleRef.current == null) {
       return;
     }
   };
@@ -16,6 +22,7 @@ function PasteBinView() {
     <>
       <Menubar />
       <div className="flex flex-col flex-grow w-2/3 m-auto mt-4 gap-2">
+        <Input ref={titleRef} placeholder="Pastebin name" />
         <Textarea ref={textAreaRef} placeholder="Paste your text here..." />
         <Button onClick={handleSubmit}>Create Paste</Button>
       </div>
