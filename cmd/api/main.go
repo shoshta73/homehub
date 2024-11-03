3c3281be9c42df7594045e7f4a3eb1b7b885832f
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-03 13:26:14 +0100

    setup api project

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	http.ListenAndServe(":3000", r)
+	http.ListenAndServeTLS(":3001", "cert.pem", "key.pem", r)
+}
