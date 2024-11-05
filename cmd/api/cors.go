dfd880c3e10efc4e3aa2c1194dd13b12e25f1286
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-05 18:39:57 +0100

    minor adition the cors settings

--- cmd/api/cors.go
+++ cmd/api/cors.go
@@ -6,6 +6,9 @@ package main
 import "github.com/go-chi/cors"
 
 var corsOptions = cors.Options{
+	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	AllowedHeaders:   []string{"*"},
+	ExposedHeaders:   []string{"*"},
 	AllowCredentials: true,
 	MaxAge:           300,
 }
--- cmd/api/cors_dev.go
+++ cmd/api/cors_dev.go
@@ -7,6 +7,9 @@ import "github.com/go-chi/cors"
 
 var corsOptions = cors.Options{
 	AllowedOrigins:   []string{"http://localhost:5173", "http://localhost:4173"},
+	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	AllowedHeaders:   []string{"*"},
+	ExposedHeaders:   []string{"*"},
 	AllowCredentials: true,
 	MaxAge:           300,
 }
