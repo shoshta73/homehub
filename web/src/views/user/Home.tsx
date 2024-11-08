5e5f00d005e2c87dacc42625b4a8bafcf8a66153
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-08 20:14:36 +0100

    implement validation on home user route

--- web/src/views/user/Home.tsx
+++ web/src/views/user/Home.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { baseUrl } from "../../helpers/api";
 
 function Home() {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const validate = async () => {
+      const res = await fetch(baseUrl() + "/auth/validate", {
+        method: "POST",
+        credentials: "include",
+      });
+      if (res.status !== 200) {
+        navigate("/login");
+      }
+    };
+
+    validate();
+  }, []);
 
   return (
     <div>
