488ce78cd562bcf73365080a94b52ae02c0504ad
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-10 13:06:09 +0100

    showing created pastes stat on the userhome

--- web/src/views/user/Home.tsx
+++ web/src/views/user/Home.tsx
@@ -1,6 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { baseUrl } from "../../helpers/api";
+
+const Stats = () => {
+  const [createdPastes, setCreatedPastes] = useState(0);
+  const [createdError, setCreatedError] = useState<string | null>(null);
+  const [createdPastesLoaded, setCreatedPastesLoaded] = useState(false);
+
+  useEffect(() => {
+    const getCreatedPastes = async () => {
+      try {
+        const res = await fetch(baseUrl() + "/pastebin/created/count", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await res.text();
+        setCreatedPastes(parseInt(data, 10));
+      } catch (err) {
+        setCreatedError(err as string);
+      } finally {
+        setCreatedPastesLoaded(true);
+      }
+    };
+
+    if (!createdPastesLoaded) getCreatedPastes();
+  }, [createdPastesLoaded]);
+
+  return (
+    <>
+      <h1>Stats</h1>
+      <h3>Pastebin</h3>
+      <p>Created</p>
+      {createdError !== null ? <>{createdError}</> : <>{createdPastes}</>}
+    </>
+  );
+};
 
 function Home() {
   const navigate = useNavigate();
@@ -25,7 +59,7 @@ function Home() {
       <div>
         <button onClick={() => navigate("/home/pastebin")}>Pastebin</button>
       </div>
-      <div>{location.pathname === "/home" ? <>Home View</> : <Outlet />}</div>
+      <div>{location.pathname === "/home" ? <Stats /> : <Outlet />}</div>
     </div>
   );
 }
